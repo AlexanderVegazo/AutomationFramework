e4c47be7577236804ad6641099cf9aa00fe219fe
Alexander Vegazo <alexander_vicente@hotmail.com>
2018-10-15 23:32:25 -0400

    changing some repos in maven

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -8,7 +8,8 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
  */
 
 //@RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/java/features"}, format = {"json:target/cucumber.json", "html:target/site/cucumber-pretty"},
+@CucumberOptions(features = {"src/test/java/features"},
+        plugin = {"json:target/cucumber.json", "html:target/site/cucumber-pretty"},
         glue = "steps")
 public class TestRunner extends AbstractTestNGCucumberTests {
 
